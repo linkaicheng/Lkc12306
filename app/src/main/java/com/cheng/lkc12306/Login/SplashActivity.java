1e581111a3c43554db1db318b395ea19c84a31b4
linkaicheng <1063002901@qq.com>
2016-08-22 20:21:30 +0800

    优化我的12306模块的一些代码

--- app/src/main/java/com/cheng/lkc12306/Login/SplashActivity.java
+++ app/src/main/java/com/cheng/lkc12306/Login/SplashActivity.java
@@ -21,14 +21,12 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 //从SharedPreferences中取值，如果取到值不为空则为自动登录，跳转到MainActivity
                 //否则为非自动登录，则跳转到LoginActivity中。
                 SharedPreferences sp=getSharedPreferences("user", Context.MODE_PRIVATE);
                 String userName=sp.getString("name","");
                 String password=sp.getString("pwd","");
                 if(TextUtils.isEmpty(userName)||TextUtils.isEmpty(password)){
-
                     Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                     //启动Activity
                     startActivity(intent);
--- app/src/main/java/com/cheng/lkc12306/fragment/MyFragment.java
+++ app/src/main/java/com/cheng/lkc12306/fragment/MyFragment.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by admin on 2016/8/17.
+ * Created by cheng on 2016/8/17.
  */
 public class MyFragment extends Fragment {
     private ListView lvMy;
@@ -42,53 +42,66 @@ public class MyFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        initView();
+
+    }
+
+    //初始化控件并为控件设置监听
+    private void initView() {
         btnLogout = (Button) getActivity().findViewById(R.id.btnLogout);
         lvMy = (ListView) getActivity().findViewById(R.id.lvMy);
-
         List<Map<String, Object>> data = getData();
         SimpleAdapter adapter = new SimpleAdapter(getActivity(), data, R.layout.item_my_main,
                 new String[]{"icon", "title"}, new int[]{R.id.imIcon, R.id.tvTitle});
         lvMy.setAdapter(adapter);
-        lvMy.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            final Intent intent = new Intent();
-                switch (position) {
-                    case 0:
-                        intent.setClass(getActivity(), ContactActivity.class);
-                        break;
-                    case 1:
-                        intent.setClass(getActivity(), AccountActivity.class);
-                        break;
-                    case 2:
-                        intent.setClass(getActivity(), PassWordActivity.class);
-                        break;
-                }
-                    startActivity(intent);
-            }
-        });
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //1、获得SharedPreferences对象
-                SharedPreferences sp=getActivity().getSharedPreferences("user", Context.MODE_PRIVATE);
-                //2.通过sp对象获得编辑器
-                SharedPreferences.Editor editor=sp.edit();
-                //调用remove()方法清除数据
-                editor.remove("name");
-                editor.remove("pwd");
-                editor.clear();
-                //提交数据
-                editor.commit();
-               Intent intent=new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
-                getActivity().finish();
-            }
-        });
-
+        lvMy.setOnItemClickListener(new LvMyOnItCkListener());
+        btnLogout.setOnClickListener(new BtnLogoutOnCkListener());
     }
 
+    //退出登录按钮的点击监听
+    private class BtnLogoutOnCkListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            //清除保存的密码
+            //1、获得SharedPreferences对象
+            SharedPreferences sp = getActivity().getSharedPreferences("user", Context.MODE_PRIVATE);
+            //2.通过sp对象获得编辑器
+            SharedPreferences.Editor editor = sp.edit();
+            //调用remove()方法清除数据
+            editor.remove("name");
+            editor.remove("pwd");
+            editor.clear();
+            //提交数据
+            editor.commit();
+            //到登录界面
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+        }
+    }
+
+    //我的12306模块功能列表的点击监听
+    private class LvMyOnItCkListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            final Intent intent = new Intent();
+            switch (position) {
+                case 0:
+                    //跳到我的联系人界面
+                    intent.setClass(getActivity(), ContactActivity.class);
+                    break;
+                case 1:
+                    //跳到我的账户界面
+                    intent.setClass(getActivity(), AccountActivity.class);
+                    break;
+                case 2:
+                    //跳到我的密码界面
+                    intent.setClass(getActivity(), PassWordActivity.class);
+                    break;
+            }
+            startActivity(intent);
+        }
+    }
 
     private List<Map<String, Object>> getData() {
         List<Map<String, Object>> data = new ArrayList<>();
