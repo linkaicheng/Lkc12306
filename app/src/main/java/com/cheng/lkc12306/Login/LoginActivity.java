35f14f554c0e7c35f38bcdcfd5e66a33279c67e7
linkaicheng <1063002901@qq.com>
2016-08-23 16:23:40 +0800

    登陆功能，连接服务器进行验证

--- app/src/main/java/com/cheng/lkc12306/Login/LoginActivity.java
+++ app/src/main/java/com/cheng/lkc12306/Login/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.cheng.lkc12306.login;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
@@ -15,8 +13,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.cheng.lkc12306.MainActivity;
 import com.cheng.lkc12306.R;
+import com.cheng.lkc12306.utils.Constant;
+import com.cheng.lkc12306.utils.Md5Utils;
+import com.cheng.lkc12306.utils.NetUtils;
+import com.cheng.lkc12306.utils.URLConnManager;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText edtUserName;
@@ -24,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
     private TextView tvLostPassword;
     private CheckBox ckLogin;
+    private ProgressDialog pDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,12 +42,13 @@ public class LoginActivity extends AppCompatActivity {
         initView();
 
     }
+
     //初始化控件并为控件设置监听
-    private void initView(){
+    private void initView() {
         edtUserName = (EditText) this.findViewById(R.id.edtUsername);
         edtPassword = (EditText) this.findViewById(R.id.edtPassword);
         btnLogin = (Button) this.findViewById(R.id.btnLogin);
-        tvLostPassword= (TextView) this.findViewById(R.id.tvLostPassword);
+        tvLostPassword = (TextView) this.findViewById(R.id.tvLostPassword);
         ckLogin = (CheckBox) this.findViewById(R.id.ckLogin);
         // 忘记密码链接
         tvLostPassword.setText(Html
@@ -45,55 +57,51 @@ public class LoginActivity extends AppCompatActivity {
 
         btnLogin.setOnClickListener(new BtnLoginOnCkListener());
     }
+
     //登录按钮的点击监听
-   private class  BtnLoginOnCkListener implements View.OnClickListener{
-       @Override
-       public void onClick(View v) {
-           //获取数据
-           String userName = edtUserName.getText().toString();
-           String password = edtPassword.getText().toString();
+    private class BtnLoginOnCkListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            //获取数据
+            final String userName = edtUserName.getText().toString();
+            final String password = edtPassword.getText().toString();
 
-           if (TextUtils.isEmpty(userName)) {
-               edtUserName.setError("用户名不能为空！");
-               edtUserName.requestFocus();
-           } else if (TextUtils.isEmpty(password)) {
-               edtPassword.setError("密码不能为空！");
-               edtPassword.requestFocus();
-           } else if("cheng".equals(userName)&&"123".equals(password)) {
+            if (TextUtils.isEmpty(userName)) {
+                edtUserName.setError("用户名不能为空！");
+                edtUserName.requestFocus();
+            } else if (TextUtils.isEmpty(password)) {
+                edtPassword.setError("密码不能为空！");
+                edtPassword.requestFocus();
+            } else {//将用户名和密码提交到服务器进行验证
+                //判断网络是否有连接
+                if(!NetUtils.check(LoginActivity.this)){
+                    Toast.makeText(LoginActivity.this, "当前网络不可用", Toast.LENGTH_SHORT).show();
+                    return;//返回，不执行后续代码
+                }
+                //显示圆形进度对话框
+                pDialog=ProgressDialog.show(LoginActivity.this,null,"正在登陆，请稍后",false,true);
+                new Thread(){
+                    @Override
+                    public void run() {
+                        try {
+                         HttpURLConnection conn= URLConnManager.getHttpURLConnection(Constant.HOST+"/Login");
+                        List<NameValuePair> paramlist=new ArrayList<NameValuePair>();
+                        paramlist.add(new BasicNameValuePair("username",userName));
+                        paramlist.add(new BasicNameValuePair("password", Md5Utils.MD5(password)));
+                            URLConnManager.postParams(conn.getOutputStream(),paramlist);
+                            conn.connect();
+                            int code=conn.getResponseCode();
+                            if(code==200){
 
-               if(ckLogin.isChecked()){ //如果被选中，则实现保存用户名和密码
-                   //1、获得SharedPreferences对象
-                   SharedPreferences sp=getSharedPreferences("user", Context.MODE_PRIVATE);
-                   //2.通过sp对象获得编辑器
-                   SharedPreferences.Editor editor=sp.edit();
-                   //3、利用editor将用户名和密码通过键值对的形式保存到user中
-                   editor.putString("name",userName);
-                   editor.putString("pwd",password);
-                   //4、提交数据
-                   editor.commit();
+                            }
 
-               }else{//如果没有选中则清除以前保存的用户名和密码
-                   //1、获得SharedPreferences对象
-                   SharedPreferences sp=getSharedPreferences("user", Context.MODE_PRIVATE);
-                   //2.通过sp对象获得编辑器
-                   SharedPreferences.Editor editor=sp.edit();
-                   //调用remove()方法清除数据
-                   editor.remove("name");
-                   editor.remove("pwd");
-                   editor.clear();
-                   //提交数据
-                   editor.commit();
-               }
-               //创建Intent对象
-               Intent intent = new Intent(LoginActivity.this,
-                       MainActivity.class);
-               startActivity(intent);
-               finish();
-           }else{
-               Toast.makeText(LoginActivity.this,"用户名或密码不正确",Toast.LENGTH_LONG).show();
-           }
-
-       }
-   }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+            }
+        }
+    }
 
 }
