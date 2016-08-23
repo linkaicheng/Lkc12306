d8e30db64aea33db8608d602f118a1445cc17f9a
linkaicheng <1063002901@qq.com>
2016-08-23 11:23:25 +0800

    增加utils下的几个工具类

---
+++ app/src/main/java/com/cheng/lkc12306/utils/Constant.java
@@ -0,0 +1,9 @@
+package com.cheng.lkc12306.utils;
+
+/**
+ * Created by cheng.
+ */
+public interface Constant {
+    String HOST = "http://10.1.44.186`:8080/My12306";
+    int CONNECTTIME=15000;
+}
