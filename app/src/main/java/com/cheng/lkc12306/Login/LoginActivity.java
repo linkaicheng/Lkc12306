5b00e3bf484a991d0507a9d819d9e61810da749a
linkaicheng <1063002901@qq.com>
2016-08-23 21:21:26 +0800

    使用服务器进行验证，完成登录功能

--- app/src/main/java/com/cheng/lkc12306/Login/LoginActivity.java
+++ app/src/main/java/com/cheng/lkc12306/Login/LoginActivity.java
@@ -1,11 +1,17 @@
 package com.cheng.lkc12306.login;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
+import android.util.Xml;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -13,6 +19,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cheng.lkc12306.MainActivity;
 import com.cheng.lkc12306.R;
 import com.cheng.lkc12306.utils.Constant;
 import com.cheng.lkc12306.utils.Md5Utils;
@@ -21,8 +28,11 @@ import com.cheng.lkc12306.utils.URLConnManager;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +44,44 @@ public class LoginActivity extends AppCompatActivity {
     private TextView tvLostPassword;
     private CheckBox ckLogin;
     private ProgressDialog pDialog;
+
+     Handler handler=new Handler(){//进行界面更新
+        @Override
+        public void handleMessage(Message msg) {
+            if(pDialog!=null){
+                pDialog.dismiss();
+            }
+            switch (msg.what) {
+                case  1:
+                    int result=msg.arg1;
+                    String jssionid= (String) msg.obj;
+                    if(0==result){
+                        edtUserName.selectAll();
+                        edtUserName.requestFocus();
+                        edtUserName.setError("用户名或密码错误");
+                    }else if(1==result){
+                        SharedPreferences sp=getSharedPreferences("user", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor=sp.edit();
+                        editor.putString("cookie",jssionid);
+                        if(ckLogin.isChecked()){
+                            editor.putString("username",edtUserName.getText().toString());
+                            editor.putString("password",edtPassword.getText().toString());
+                        }else{
+                            editor.remove("username");
+                            editor.remove("password");
+                        }
+                        editor.commit();
+                        Intent intent=new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    break;
+                case  0:
+                    Toast.makeText(LoginActivity.this, "服务器忙。。。。", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,30 +126,71 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "当前网络不可用", Toast.LENGTH_SHORT).show();
                     return;//返回，不执行后续代码
                 }
+
+
                 //显示圆形进度对话框
                 pDialog=ProgressDialog.show(LoginActivity.this,null,"正在登陆，请稍后",false,true);
                 new Thread(){
                     @Override
                     public void run() {
+                        Message msg=new Message();
+                        HttpURLConnection conn= URLConnManager.getHttpURLConnection(Constant.HOST+"/Login");
                         try {
-                         HttpURLConnection conn= URLConnManager.getHttpURLConnection(Constant.HOST+"/Login");
                         List<NameValuePair> paramlist=new ArrayList<NameValuePair>();
                         paramlist.add(new BasicNameValuePair("username",userName));
                         paramlist.add(new BasicNameValuePair("password", Md5Utils.MD5(password)));
                             URLConnManager.postParams(conn.getOutputStream(),paramlist);
                             conn.connect();
                             int code=conn.getResponseCode();
+                           // Log.e("cheng","*********"+code);
                             if(code==200){
 
+                                InputStream is=conn.getInputStream();
+                                XmlPullParser parser= Xml.newPullParser();
+                                parser.setInput(is,"UTF-8");
+                                int eventType=parser.getEventType();
+                                String xmlResult=null;
+                                while(eventType!=XmlPullParser.END_DOCUMENT) {
+
+                                    switch (eventType) {
+                                        case  XmlPullParser.START_TAG:
+                                            if("result".equals(parser.getName())){
+                                                xmlResult=parser.nextText();
+                                            }
+                                        break;
+                                    }
+                                    eventType=parser.next();
+                                }
+                                //Log.e("cheng","********"+xmlResult);
+                                //关闭连接
+                                conn.disconnect();
+                                String cookieValue=null;
+                                String cookie=conn.getHeaderField("Set-Cookie");
+                                cookieValue=cookie.substring(0,cookie.indexOf(";"));
+                                msg.what=1;
+                                msg.arg1=Integer.parseInt(xmlResult);
+                                msg.obj=cookieValue;
+
+
+                            }else{
+                                msg.what=2;
                             }
 
                         } catch (IOException e) {
                             e.printStackTrace();
+                            msg.what=2;
+                        } catch (XmlPullParserException e) {
+                            e.printStackTrace();
+                            msg.what=2;
                         }
+                        handler.sendMessage(msg);
                     }
+
                 }.start();
             }
         }
     }
+
+
 
 }
--- app/src/main/java/com/cheng/lkc12306/utils/Constant.java
+++ app/src/main/java/com/cheng/lkc12306/utils/Constant.java
@@ -4,6 +4,6 @@ package com.cheng.lkc12306.utils;
  * Created by cheng.
  */
 public interface Constant {
-    String HOST = "http://10.1.44.186`:8080/My12306";
+    String HOST = "http://192.168.3.9:8080/My12306";
     int CONNECTTIME=15000;
 }
