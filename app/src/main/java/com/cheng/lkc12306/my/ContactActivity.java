bf9b8400323038e4ab538c9c018eed3163ffbbf7
linkaicheng <1063002901@qq.com>
2016-08-24 11:08:16 +0800

    注释ContactActivity

--- app/src/main/java/com/cheng/lkc12306/my/ContactActivity.java
+++ app/src/main/java/com/cheng/lkc12306/my/ContactActivity.java
@@ -45,7 +45,9 @@ public class ContactActivity extends AppCompatActivity {
             }
             switch (msg.what) {
                 case 1:
+                    //获取联系人列表
                     Passenger[] passengers= (Passenger[]) msg.obj;
+                    //将联系人列表更新到界面
                     for(Passenger passenger:passengers){
                         Map<String,Object> row=new HashMap<>();
                         row.put("name",passenger.getName()+"("+passenger.getType()+")");
@@ -53,6 +55,7 @@ public class ContactActivity extends AppCompatActivity {
                         row.put("tel","电话号码:"+passenger.getTel());
                         data.add(row);
                     }
+                    //每次回到联系人界面都更新一下数据
                     adapter.notifyDataSetChanged();
                     break;
                 case 2:
@@ -80,7 +83,7 @@ public class ContactActivity extends AppCompatActivity {
         lvContact.setAdapter(adapter);
         lvContact.setOnItemClickListener(new lvContactOnItListener());
     }
-
+//重写onResume方法，执行联系人数据的获取操作
     @Override
     protected void onResume() {
         super.onResume();
@@ -88,7 +91,7 @@ public class ContactActivity extends AppCompatActivity {
         if (!NetUtils.check(ContactActivity.this)) {
             Toast.makeText(ContactActivity.this, "网络不可用", Toast.LENGTH_SHORT).show();
         }
-        //显示圆形进度对话框
+        //显示进度对话框
         pDialog = ProgressDialog.show(ContactActivity.this, null, "请稍后", false, true);
         new Thread() {
             @Override
@@ -103,7 +106,9 @@ public class ContactActivity extends AppCompatActivity {
                     conn.connect();
                     int code = conn.getResponseCode();
                     if (code == 200) {
+                        //获取输入流
                         InputStream inputStream = conn.getInputStream();
+                        //将流转成字符串
                         String response = URLConnManager.converStreamToString(inputStream);
                         //利用Gson解析Json数据
                         Gson gson = new Gson();
@@ -111,10 +116,10 @@ public class ContactActivity extends AppCompatActivity {
                         msg.what = 1;
                         msg.obj = passengers;
                         inputStream.close();
-
                     } else {
                         msg.what = 2;
                     }
+                    //断开连接
                     conn.disconnect();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -127,12 +132,15 @@ public class ContactActivity extends AppCompatActivity {
     }
 
 
-
+//联系人列表的点击监听
     private class lvContactOnItListener implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //点击后跳转到相应的编辑界面
             Intent intent = new Intent(ContactActivity.this, ContactEditActivity.class);
+            //获取相应联系人的信息
             Map<String, Object> contact = data.get(position);
+            //将联系人的信息以序列化的方式保存到intent
             intent.putExtra("contact", (Serializable) contact);
             startActivity(intent);
         }
