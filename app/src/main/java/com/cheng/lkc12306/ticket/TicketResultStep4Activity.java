2508ec05fcae1924982f0692ec1143804de8105e
linkaicheng <1063002901@qq.com>
2016-08-30 00:29:32 +0800

    订单提交UI,未完成

---
+++ app/src/main/java/com/cheng/lkc12306/ticket/TicketResultStep4Activity.java
@@ -0,0 +1,15 @@
+package com.cheng.lkc12306.ticket;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cheng.lkc12306.R;
+
+public class TicketResultStep4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_result_step4);
+    }
+}
